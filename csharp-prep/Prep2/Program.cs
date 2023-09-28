7fae67676fd6d9738b3f3d8a2832d77068ff4778
happylomas <prince56ebere@gmail.com>
2023-09-27 20:27:44 -0500

    prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        // This code demostract the us of if conditional statment
+
+        Console.Write("What is your grade in percentage?");
+        string scorePercent = Console.ReadLine();
+        int percent = int.Parse(scorePercent);
+
+        String grade = "";
+
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else 
+        {
+            grade = "F";
+        }
+
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed! keep it up");
+        }
+        else
+        {
+            Console.WriteLine("Try hader next time");
+        }
+
     }
 }
